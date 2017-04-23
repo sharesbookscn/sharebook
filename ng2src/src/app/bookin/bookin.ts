cadd54a50235fbd2a04c481c9e1889fd3f99f1bf
sharesbookscn <payallmoney@163.com>
2017-04-23 18:56:39 +0800

    1,修正打开目录的脚本

--- ng2src/src/app/bookin/bookin.ts
+++ ng2src/src/app/bookin/bookin.ts
@@ -44,7 +44,7 @@ export class BookInComponent {
                 torchOn: true, // Android, launch with the torch switched on (if available)
                 prompt: "请对准条形码", // Android
                 resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-                formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+                formats: "EAN_13", // default: all but PDF_417 and RSS_EXPANDED
                 orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
                 disableAnimations: true, // iOS
                 disableSuccessBeep: false // iOS
